98676229361daf23cf1fd2fc445135006ae1866b
ducviet5138 <ducviet5138@gmail.com>
2024-09-05 13:23:34 +0700

    feat(lap-149): imple active state for tab

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -7,6 +7,16 @@ import IconMapTab from '~/assets/images/tab-map.svg';
 import IconVocabularyTab from '~/assets/images/tab-vocabulary.svg';
 import IconProfileTab from '~/assets/images/tab-profile.svg';
 import { useAuth } from '~/hooks/zustand';
+import { Platform, View } from 'react-native';
+import { SvgProps } from 'react-native-svg';
+
+function ActiveTabIcon({ icon: Icon, focused } : { icon: React.FC<SvgProps>, focused: Boolean }) {
+  return (
+    <View className={`${focused ? 'm-4 border border-orange-500 bg-orange-50 rounded' : ''}`}>
+      <Icon />
+    </View>
+  );
+}
 
 export default function TabsLayout() {
   const { status } = useAuth();
@@ -24,37 +34,30 @@ export default function TabsLayout() {
   }, [hideSplash, status]);
 
   return (
-    <Tabs initialRouteName='(map)' screenOptions={{ tabBarShowLabel: false, headerShown: false, tabBarStyle: { height: 64 } }}>
-      <Tabs.Screen
-        name='practice'
-        options={{
-          tabBarIcon: IconPracticeTab,
-        }}
-      />
-      <Tabs.Screen
-        name='mission'
-        options={{
-          tabBarIcon: IconMissionTab,
-        }}
-      />
-      <Tabs.Screen
-        name='(map)'
-        options={{
-          tabBarIcon: IconMapTab,
-        }}
-      />
-      <Tabs.Screen
-        name='vocabulary'
-        options={{
-          tabBarIcon: IconVocabularyTab,
-        }}
-      />
-      <Tabs.Screen
-        name='profile'
-        options={{
-          tabBarIcon: IconProfileTab,
-        }}
-      />
+    <Tabs
+      initialRouteName='(map)'
+      screenOptions={({ route }) => ({
+        tabBarShowLabel: false,
+        headerShown: false,
+        tabBarStyle: { height: Platform.OS === 'ios' ? 101 : 80 },
+        tabBarIcon: ({ focused }) => {
+          const iconMapping: { [key: string]: React.FC<SvgProps> } = {
+            'practice': IconPracticeTab,
+            'mission': IconMissionTab,
+            '(map)': IconMapTab,
+            'vocabulary': IconVocabularyTab,
+            'profile': IconProfileTab,
+          }
+
+          return <ActiveTabIcon icon={iconMapping[route.name]} focused={focused} />;
+        },
+      })}
+    >
+      <Tabs.Screen name='practice' />
+      <Tabs.Screen name='mission' />
+      <Tabs.Screen name='(map)' />
+      <Tabs.Screen name='vocabulary' />
+      <Tabs.Screen name='profile' />
     </Tabs>
   );
 }
