ed57e1ad5e2b6a02e6710f70a8dc42c5242ea14a
ducviet5138 <ducviet5138@gmail.com>
2024-09-27 06:22:44 +0700

    fix(lap-189): resolve cmts

--- app/lesson/explanation/index.tsx
+++ app/lesson/explanation/index.tsx
@@ -91,7 +91,6 @@ export default function Explanation() {
           <Button
             size={'lg'}
             variant={'black'}
-            className=''
             onPress={() => {
               if (router.canGoBack()) {
                 router.back();
