ae263520e7f56f46129130b3eef6a2c37f4aec39
vmathu <anhthuvominh1@gmail.com>
2024-10-12 01:56:53 +0700

    feat(lap-197): change carousel lib

--- app/exercise/[exerciseId]/[questionTypeId]/index.tsx
+++ app/exercise/[exerciseId]/[questionTypeId]/index.tsx
@@ -2,8 +2,8 @@ import { router, useLocalSearchParams } from 'expo-router';
 import { BookMarked, ChevronLeft, ChevronRight, LucidePlay } from 'lucide-react-native';
 import { useEffect, useRef, useState } from 'react';
 import { useTranslation } from 'react-i18next';
-import { Dimensions, Image, Text, TouchableOpacity, View } from 'react-native';
-import Carousel, { ICarouselInstance } from 'react-native-reanimated-carousel';
+import { Image, Text, TouchableOpacity, View } from 'react-native';
+import PagerView from 'react-native-pager-view';
 import { SafeAreaView } from 'react-native-safe-area-context';
 
 import { Loading } from '~/components/molecules/Loading';
@@ -14,8 +14,52 @@ import { Text as UIText } from '~/components/ui/Text';
 import { useListLessons, useQuestionTypes } from '~/hooks/react-query/useDailyLesson';
 import { useDailyLesson } from '~/hooks/zustand';
 import { BandScoreEnum, SkillEnum } from '~/lib/enums';
-import { IQuestionType } from '~/lib/interfaces';
+import { ILesson, IQuestionType } from '~/lib/interfaces';
 import { formatLearningDuration } from '~/lib/utils';
+
+function Card({
+  t,
+  item,
+  lessons,
+  handlePrev,
+  handleNext,
+}: {
+  readonly t: (key: string) => string;
+  readonly item: ILesson;
+  readonly lessons: ILesson[];
+  readonly handlePrev: () => void;
+  readonly handleNext: () => void;
+}) {
+  return (
+    <View className='w-full gap-2 rounded-lg bg-neutral-50 px-4 py-5'>
+      <View className='flex-row justify-between'>
+        <TouchableOpacity onPress={handlePrev} disabled={item.order === 1}>
+          <ChevronLeft size={24} color={item.order === 1 ? 'grey' : 'black'} />
+        </TouchableOpacity>
+        <Text className='font-isemibold text-title-3'>
+          {item.order}/{lessons.length}
+        </Text>
+        <TouchableOpacity onPress={handleNext} disabled={item.order === lessons.length}>
+          <ChevronRight size={24} color={item.order === lessons.length ? 'grey' : 'black'} />
+        </TouchableOpacity>
+      </View>
+      <View className='gap-2'>
+        <View className='h-12 w-12 rounded-full bg-white p-3'>
+          <BookMarked size={24} color='black' />
+        </View>
+        <Text className='z-10 font-ibold text-title-1'>{item.name}</Text>
+      </View>
+      <View className='flex-row items-center gap-4'>
+        <View className='h-2 flex-grow'>
+          <Progress value={(item.xp / 50) * 100} />
+        </View>
+        <Text className='font-imedium text-subhead text-supporting-text'>
+          {t('questionTypes.xp')} {item.xp}/50
+        </Text>
+      </View>
+    </View>
+  );
+}
 
 export default function QuestionType() {
   const { exerciseId, questionTypeId } = useLocalSearchParams<{ exerciseId: string; questionTypeId: string }>();
@@ -27,29 +71,29 @@ export default function QuestionType() {
   setCurrentQuestionType(currentQuestionType ? currentQuestionType : ({} as IQuestionType));
   const { bandScore } = currentQuestionType?.progress || { bandScore: 'pre_ielts' };
   const { data: lessons, isLoading: lessonsLoading } = useListLessons({ questionTypeId });
-  const width = Dimensions.get('window').width - 32;
-  const ref = useRef<ICarouselInstance>(null);
   const { t } = useTranslation('translation');
-  const currentLesson = lessons?.lessons?.find((lesson) => lesson.isCurrent) || lessons?.lessons?.[0];
 
-  const [curLessonId, setCurLessonId] = useState<number>(currentLesson?.id || 0);
+  const ref = useRef<PagerView>(null);
+  const [currentLesson, setCurrentLesson] = useState<ILesson | undefined>(
+    lessons?.lessons.find((l) => l.isCurrent) || lessons?.lessons[0]
+  );
 
   useEffect(() => {
-    if (currentLesson) {
-      setCurLessonId(currentLesson.id);
-    }
-  }, [currentLesson]);
+    const lesson = lessons?.lessons.find((l) => l.isCurrent) || lessons?.lessons[0];
+    setCurrentLesson(lesson);
+    ref.current?.setPage((lesson?.order || 1) - 1);
+  }, [lessons]);
 
   if (lessonsLoading) {
     return <Loading />;
   }
 
   const handlePrev = () => {
-    ref.current?.prev();
+    ref.current?.setPage((currentLesson?.order || 1) - 2);
   };
 
   const handleNext = () => {
-    ref.current?.next();
+    ref.current?.setPage(currentLesson?.order || 1);
   };
 
   return (
@@ -80,52 +124,32 @@ export default function QuestionType() {
             <Text className='font-isemibold text-subhead'>{t('questionType.theoryPractice')}</Text>
           </Button>
         </View>
-        <View className='flex-grow flex-row items-center'>
-          {lessons && lessons.lessons.length > 0 ? (
-            <Carousel
-              loop={false}
-              ref={ref}
-              width={width}
-              height={210}
-              data={lessons?.lessons || []}
-              defaultIndex={(currentLesson && currentLesson.order - 1) || 0}
-              onSnapToItem={(index) => setCurLessonId(lessons?.lessons[index].id)}
-              renderItem={({ item }) => (
-                <View className='w-full gap-2 rounded-lg bg-neutral-50 px-4 py-5'>
-                  <View className='flex-row justify-between'>
-                    <TouchableOpacity onPress={handlePrev} disabled={item.order === 1}>
-                      <ChevronLeft size={24} color={item.order === 1 ? 'grey' : 'black'} />
-                    </TouchableOpacity>
-                    <Text className='font-isemibold text-title-3'>
-                      {item.order}/{lessons.lessons.length}
-                    </Text>
-                    <TouchableOpacity onPress={handleNext} disabled={item.order === lessons.lessons.length}>
-                      <ChevronRight size={24} color={item.order === lessons.lessons.length ? 'grey' : 'black'} />
-                    </TouchableOpacity>
-                  </View>
-                  <View className='gap-2'>
-                    <View className='h-12 w-12 rounded-full bg-white p-3'>
-                      <BookMarked size={24} color='black' />
-                    </View>
-                    <Text className='z-10 font-ibold text-title-1'>{item.name}</Text>
-                  </View>
-                  <View className='flex-row items-center gap-4'>
-                    <View className='h-2 flex-grow'>
-                      <Progress value={(item.xp / 50) * 100} />
-                    </View>
-                    <Text className='font-imedium text-subhead text-supporting-text'>
-                      {t('questionTypes.xp')} {item.xp}/50
-                    </Text>
-                  </View>
-                </View>
-              )}
-            />
+        <View className='flex-grow flex-row items-center justify-center'>
+          {lessons?.lessons.length ? (
+            <PagerView
+              style={{ flex: 1, height: 210 }}
+              initialPage={(currentLesson?.order || 1) - 1}
+              pageMargin={16}
+              orientation='horizontal'
+              onPageSelected={(e) => setCurrentLesson(lessons.lessons[e.nativeEvent.position])}
+              ref={ref}>
+              {lessons.lessons.map((lesson) => (
+                <Card
+                  key={lesson.id}
+                  t={t}
+                  item={lesson}
+                  lessons={lessons.lessons}
+                  handlePrev={handlePrev}
+                  handleNext={handleNext}
+                />
+              ))}
+            </PagerView>
           ) : (
-            <Text>Không có bài học nào</Text>
+            <Text>{t('questionType.noLessonFound')}</Text>
           )}
         </View>
         <View className='mb-18 gap-4'>
-          <Button size='lg' onPress={() => router.push(`/lesson/${curLessonId}`)} disabled={!curLessonId}>
+          <Button size='lg' onPress={() => router.push(`/lesson/${currentLesson?.id || 0}`)} disabled={!currentLesson}>
             <UIText>{t('questionType.practiceBtn')}</UIText>
           </Button>
           <Button size='lg' className='bg-neutral-900'>
