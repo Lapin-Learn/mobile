214a1e5af16165afd1c637ba22461a0b96488501
MinEn1712 <tranminhanh1912@gmail.com>
2024-10-15 16:21:19 +0700

    widget onclick to app

--- android/app/src/main/java/com/datn/lapinlearn/LapinLearnWidget.kt
+++ android/app/src/main/java/com/datn/lapinlearn/LapinLearnWidget.kt
@@ -5,12 +5,15 @@ import android.appwidget.AppWidgetManager
 import android.appwidget.AppWidgetProvider
 import android.content.Context
 import android.content.Intent
+import android.net.Uri
 import android.widget.RemoteViews
 import org.json.JSONException
 import org.json.JSONObject
 
 
 class LapinLearnWidget : AppWidgetProvider() {
+    val REFRESH_ACTION: String = "com.datn.lapinlearn.REFRESH_ACTION"
+
     override fun onUpdate(
         context: Context,
         appWidgetManager: AppWidgetManager,
@@ -19,18 +22,7 @@ class LapinLearnWidget : AppWidgetProvider() {
         // There may be multiple widgets active, so update all of them
         for (appWidgetId in appWidgetIds) {
             updateAppWidget(context, appWidgetManager, appWidgetId)
-//            val intent = Intent(context, MainActivity::class.java)
-//            intent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, appWidgetId)
-//
-//            val pendingIntent = PendingIntent.getActivity(context, 0, intent,
-//                PendingIntent.FLAG_IMMUTABLE)
-//
-//            val views = RemoteViews(context.packageName, R.layout.lapin_learn_widget)
-//            views.setOnClickPendingIntent(R.id.appwidgetText, pendingIntent)
-//            appWidgetManager.updateAppWidget(appWidgetId, views)
         }
-
-
     }
 
     override fun onEnabled(context: Context) {
@@ -52,8 +44,35 @@ internal fun updateAppWidget(
         val appString = sharedPref.getString("appData", "{\"text\":'no data'}")
         val appData = JSONObject(appString.toString())
         val views = RemoteViews(context.packageName, R.layout.lapin_learn_widget)
+
+        val intent = Intent(context, MainActivity::class.java)
+        intent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, appWidgetId)
+        val pendingIntent = PendingIntent.getActivity(context, 0, intent,
+            PendingIntent.FLAG_IMMUTABLE)
+
+        views.setOnClickPendingIntent(R.id.appwidgetContainer, pendingIntent)
         views.setTextViewText(R.id.appwidgetText, appData.getString("text"))
         appWidgetManager.updateAppWidget(appWidgetId, views)
+
+
+        val rv = RemoteViews(context.packageName, R.layout.widget_layout)
+
+        val intentServiceCall = Intent(
+            context,
+            WidgetService::class.java
+        )
+        intentServiceCall.putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, appWidgetIds.get(i))
+
+
+        //Refresh
+        val refreshIntent = Intent(context, LapinLearnWidget::class.java)
+        refreshIntent.setAction(LapinLearnWidget.REFRESH_ACTION)
+        refreshIntent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_IDS, appWidgetIds)
+        intentServiceCall.setData(Uri.parse(intentServiceCall.toUri(Intent.URI_INTENT_SCHEME)))
+        val pendingIntent =
+            PendingIntent.getBroadcast(context, 0, refreshIntent, PendingIntent.FLAG_UPDATE_CURRENT)
+        rv.setOnClickPendingIntent(R.id.ivRefreshWidget, pendingIntent)
+
     } catch (e: JSONException) {
         e.printStackTrace()
     }
