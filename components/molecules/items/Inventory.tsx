560ebdc5d9bc2390e74df7e9850409bdfd909b8f
HenryDo203 <dophamthanhuy2842003@gmail.com>
2024-11-10 01:12:52 +0700

    feat(lap-397): inventory UI

--- components/molecules/items/Inventory.tsx
+++ components/molecules/items/Inventory.tsx
@@ -1,31 +1,14 @@
-import { useState } from 'react';
 import { useTranslation } from 'react-i18next';
-import { StyleSheet, Text, View } from 'react-native';
+import { Image, Pressable, ScrollView, StyleSheet, Text, View } from 'react-native';
 
-import InventoryIcon from '~/assets/images/items/Inventory.svg';
-import InventoryActiveIcon from '~/assets/images/items/InventoryActive.svg';
-import ShopIcon from '~/assets/images/items/Shop.svg';
-import ShopActiveIcon from '~/assets/images/items/ShopActive.svg';
-import { Inventory } from '~/components/molecules/items/Inventory';
-import { NavigationBar } from '~/components/molecules/NavigationBar';
-import PlatformView from '~/components/templates/PlatformView';
-import { Button } from '~/components/ui/Button';
 import Styles from '~/constants/GlobalStyles';
-import { ShopItemEnum } from '~/lib/enums';
-import { IShop } from '~/lib/types';
-
-const items: IShop[] = [
+const items = [
   {
     id: '7f151944-2085-469d-b7b0-e8664b79848e',
-    name: 'STREAK_FREEZE' as ShopItemEnum,
+    name: 'STREAK_FREEZE',
     description: 'Streak Freeze',
-    price: {
-      '1': 200,
-    },
-    duration: 0,
+    amount: 1,
     imageId: 'b51ae487-d2a8-4592-986f-fc444c84b9e9',
-    createdAt: '2024-11-09T03:07:41.584Z',
-    updatedAt: '2024-11-09T03:07:41.584Z',
     image: {
       id: 'b51ae487-d2a8-4592-986f-fc444c84b9e9',
       name: 'streak_freeze.png',
@@ -33,20 +16,13 @@ const items: IShop[] = [
       permission: 'public',
       url: 'https://vou.453294f7d0377a0acaec90d3a0ff135c.r2.cloudflarestorage.com/b51ae487-d2a8-4592-986f-fc444c84b9e9?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Credential=61bb1234d6eabb97b2c3fddc8f0f5fb5%2F20241109%2Fauto%2Fs3%2Faws4_request&X-Amz-Date=20241109T082747Z&X-Amz-Expires=3600&X-Amz-Signature=acd1db273999f20f1b001aa0da4b106aad16821f0fba32f6cbde0f0fa11a96ae&X-Amz-SignedHeaders=host&response-content-disposition=inline%3B%20filename%3Dstreak_freeze.png&x-id=GetObject',
     },
-    popular: '1',
-    isPopular: false,
   },
   {
     id: '0a1fbe80-0f8e-47f9-9f70-d718b76147bf',
-    name: 'RANDOM_GIFT' as ShopItemEnum,
+    name: 'RANDOM_GIFT',
     description: 'Quà tặng ngẫu nhiên',
-    price: {
-      '1': 150,
-    },
-    duration: 0,
+    amount: 5,
     imageId: '710b936c-ee1e-419c-9385-007d6bd36faf',
-    createdAt: '2024-11-09T03:07:41.584Z',
-    updatedAt: '2024-11-09T03:07:41.584Z',
     image: {
       id: '710b936c-ee1e-419c-9385-007d6bd36faf',
       name: 'random_gift.png',
@@ -54,19 +30,12 @@ const items: IShop[] = [
       permission: 'public',
       url: 'https://vou.453294f7d0377a0acaec90d3a0ff135c.r2.cloudflarestorage.com/710b936c-ee1e-419c-9385-007d6bd36faf?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Credential=61bb1234d6eabb97b2c3fddc8f0f5fb5%2F20241109%2Fauto%2Fs3%2Faws4_request&X-Amz-Date=20241109T082747Z&X-Amz-Expires=3600&X-Amz-Signature=e33dc9416405cc3db57f52859eb364532df995ea4cb48a20ae9f06791204e71d&X-Amz-SignedHeaders=host&response-content-disposition=inline%3B%20filename%3Drandom_gift.png&x-id=GetObject',
     },
-    popular: '1',
-    isPopular: false,
   },
   {
     id: 'b59174e5-5d0b-48d6-895c-b0bbeafe577b',
-    name: 'ULTIMATE_TIME' as ShopItemEnum,
+    name: 'ULTIMATE_TIME',
     description: 'Thời gian siêu cấp',
-    price: {
-      '1': 100,
-      '5': 400,
-      '15': 1000,
-    },
-    duration: 15,
+    amount: 3,
     imageId: 'f074c116-d74a-440b-8d18-796e9e115822',
     createdAt: '2024-11-09T03:07:41.584Z',
     updatedAt: '2024-11-09T03:07:41.584Z',
@@ -77,18 +46,13 @@ const items: IShop[] = [
       permission: 'public',
       url: 'https://vou.453294f7d0377a0acaec90d3a0ff135c.r2.cloudflarestorage.com/f074c116-d74a-440b-8d18-796e9e115822?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Credential=61bb1234d6eabb97b2c3fddc8f0f5fb5%2F20241109%2Fauto%2Fs3%2Faws4_request&X-Amz-Date=20241109T082747Z&X-Amz-Expires=3600&X-Amz-Signature=0b370dff29868afa60f48f5c23e198f00259776ffded42b59c36d9de52cce6ae&X-Amz-SignedHeaders=host&response-content-disposition=inline%3B%20filename%3Dultimate_time.png&x-id=GetObject',
     },
-    popular: '1',
-    isPopular: false,
   },
+
   {
     id: 'ca8499e3-c7c1-43fe-855e-b6ba45a2fa6d',
-    name: 'IDENTIFICATION' as ShopItemEnum,
+    name: 'IDENTIFICATION',
     description: 'Nhận diện',
-    price: {
-      '1': 20,
-      '5': 80,
-      '15': 200,
-    },
+    amount: 4,
     duration: 0,
     imageId: '458f1b59-5304-48d4-89e5-c4b5e1782dac',
     createdAt: '2024-11-09T03:07:41.584Z',
@@ -100,71 +64,59 @@ const items: IShop[] = [
       permission: 'public',
       url: 'https://vou.453294f7d0377a0acaec90d3a0ff135c.r2.cloudflarestorage.com/458f1b59-5304-48d4-89e5-c4b5e1782dac?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Credential=61bb1234d6eabb97b2c3fddc8f0f5fb5%2F20241109%2Fauto%2Fs3%2Faws4_request&X-Amz-Date=20241109T082747Z&X-Amz-Expires=3600&X-Amz-Signature=dfff75e7405a6c4385b034bc55054b27a34eeda1a5ecbbd23741836ff99a26ba&X-Amz-SignedHeaders=host&response-content-disposition=inline%3B%20filename%3Didentification.png&x-id=GetObject',
     },
-    popular: '2',
-    isPopular: true,
   },
 ];
-
-const ItemTabs = ({ isShop, setIsShop }: { isShop: boolean; setIsShop: (isShop: boolean) => void }) => {
-  const { t } = useTranslation();
+export const Inventory = () => {
   return (
-    <View style={styles.tabContainer}>
-      <Button
-        size='lg'
-        variant='link'
-        style={[styles.buttonContainer, isShop ? { borderBottomWidth: 2, ...Styles.borderColor.orange[500] } : {}]}
-        onPress={() => setIsShop(true)}>
-        {isShop ? <ShopActiveIcon /> : <ShopIcon />}
-        <Text style={[styles.buttonText, isShop ? { ...Styles.color.orange[500] } : { ...Styles.color.black }]}>
-          {t('Shop')}
-        </Text>
-      </Button>
-      <Button
-        size='lg'
-        variant='link'
-        style={[styles.buttonContainer, !isShop ? { borderBottomWidth: 2, ...Styles.borderColor.orange[500] } : {}]}
-        onPress={() => setIsShop(false)}>
-        {!isShop ? <InventoryActiveIcon /> : <InventoryIcon />}
-        <Text style={[styles.buttonText, !isShop ? { ...Styles.color.orange[500] } : { ...Styles.color.black }]}>
-          {t('Inventory')}
-        </Text>
-      </Button>
-    </View>
+    <ScrollView>
+      <View style={styles.container}>
+        {items.map((item) => (
+          <Item key={item.id} item={item} />
+        ))}
+      </View>
+    </ScrollView>
   );
 };
 
-const Items = () => {
-  const [isShop, setIsShop] = useState(true);
-
+const Item = ({ item }: any) => {
+  const { t } = useTranslation('item');
   return (
-    <PlatformView>
-      <NavigationBar headerTitle='Items' headerLeftShown />
-      <ItemTabs isShop={isShop} setIsShop={setIsShop} />
-      <View style={styles.itemView}>{isShop ? <Text>Shop</Text> : <Inventory />}</View>
-    </PlatformView>
+    <Pressable style={styles.itemButton}>
+      <View style={{ paddingVertical: 17.25 }}>
+        <Image source={{ uri: item.image.url }} style={{ width: 80, height: 80 }} />
+      </View>
+      <View style={{ justifyContent: 'center', alignItems: 'center', gap: 4, flexWrap: 'wrap' }}>
+        <Text style={{ ...Styles.fontSize.subhead, ...Styles.font.semibold, textAlign: 'center' }}>
+          {t(`shop.items.${item.name}.name`, {})}
+        </Text>
+        <Text style={{ ...Styles.fontSize['caption-1'], ...Styles.font.normal }}>
+          {t('inventory.Amount', { amount: item.amount })}
+        </Text>
+      </View>
+    </Pressable>
   );
 };
 
 const styles = StyleSheet.create({
-  tabContainer: {
-    flexDirection: 'row',
-    justifyContent: 'space-around',
-    paddingHorizontal: 16,
-  },
-  itemView: {
-    display: 'flex',
-    height: '100%',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  buttonContainer: {
+  container: {
+    width: '100%',
     flex: 1,
     flexDirection: 'row',
-    gap: 8,
-    marginHorizontal: 10,
-    borderRadius: 0,
+    flexWrap: 'wrap',
+    justifyContent: 'center',
+    alignItems: 'center',
+    columnGap: 16,
+    rowGap: 16,
+    marginVertical: 16,
   },
-  buttonText: { ...Styles.font.semibold, ...Styles.fontSize.subhead },
+  itemButton: {
+    flexBasis: '45%',
+    padding: 8,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 8,
+    borderWidth: 1,
+    margin: 0,
+    ...Styles.borderColor.neutral[100],
+  },
 });
-
-export default Items;
--- locales/vi/index.ts
+++ locales/vi/index.ts
@@ -1,11 +1,11 @@
 import auth from './auth.json';
+import item from './item.json';
 import lesson from './lesson.json';
 import milestone from './milestone.json';
 import mission from './mission.json';
 import profile from './profile.json';
 import question from './question.json';
-import shop from './shop.json';
 import validation from './validation.json';
 import vietnamese from './vietnamese.json';
 
-export { auth, lesson, milestone, mission, profile, question, shop, validation, vietnamese };
+export { auth, item, lesson, milestone, mission, profile, question, validation, vietnamese };
