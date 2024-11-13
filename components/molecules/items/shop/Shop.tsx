8b4e40bcfb03fa92601bc8c8d02c52a9679bef69
vmathu <anhthuvominh1@gmail.com>
2024-11-14 01:16:17 +0700

    fix(lap-424): missing pass popular prop

--- components/molecules/items/shop/Shop.tsx
+++ components/molecules/items/shop/Shop.tsx
@@ -14,7 +14,14 @@ export const Shop = () => {
       style={{ padding: 16, width: '100%', flex: 1 }}
       data={data}
       renderItem={({ item }) => (
-        <ItemCard key={item.id} id={item.id} name={item.name} image={item.image.url} price={item.price} />
+        <ItemCard
+          key={item.id}
+          id={item.id}
+          name={item.name}
+          image={item.image.url}
+          price={item.price}
+          popular={item.popular}
+        />
       )}
       keyExtractor={(item) => item.id}
       ItemSeparatorComponent={() => <View style={{ height: 20 }} />}
