d398fcd8b2e04dbd30ad9d750eb016f926e28a3c
ducviet5138 <ducviet5138@gmail.com>
2024-11-15 15:35:22 +0700

    build: add github action

--- services/axios/auth.ts
+++ services/axios/auth.ts
@@ -10,6 +10,7 @@ import { deleteFcmToken } from './notification';
 const GOOGLE_CLIENT_ID = process.env.EXPO_PUBLIC_FIREBASE_GOOGLE_CLIENT_ID;
 GoogleSignin.configure({
   webClientId: GOOGLE_CLIENT_ID,
+  offlineAccess: true, // DO NOT REMOVE THIS LINE
 });
 
 export type Session = {
