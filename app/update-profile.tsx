f5b397a98e1e6605f20117f6be94c54ef0b7f5d4
Huy203 <95161186+Huy203@users.noreply.github.com>
2024-11-16 18:51:54 +0700

    fix(lap-439): fix loop when incomplete update profile screen and warn when reset state (#105)

--- app/update-profile.tsx
+++ app/update-profile.tsx
@@ -67,8 +67,6 @@ const UpdateProfile = () => {
       <NavigationBar title={t('profile.basic_info')} />
       <View
         style={{
-          height: '100%',
-          flexDirection: 'column',
           flexGrow: 1,
           justifyContent: 'space-between',
           alignItems: 'center',
