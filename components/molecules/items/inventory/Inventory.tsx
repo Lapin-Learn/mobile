4ee6714f07aef3b9350dd79dcc5f22ae14113c6c
vmathu <anhthuvominh1@gmail.com>
2024-12-09 00:49:51 +0700

    fix(lap-561): inventory layout and filter data

--- components/molecules/items/inventory/Inventory.tsx
+++ components/molecules/items/inventory/Inventory.tsx
@@ -1,44 +1,16 @@
-import { useMemo } from 'react';
 import { useTranslation } from 'react-i18next';
 import { FlatList, Text, View } from 'react-native';
 
 import InventoryEmpty from '~/assets/images/items/InventoryEmpty.svg';
 import Styles from '~/constants/GlobalStyles';
 import { useInventory } from '~/hooks/react-query/useItem';
-import { ItemEnum } from '~/lib/enums';
-import { IInventory } from '~/lib/types';
 
 import { Loading } from '../../Loading';
 import { Item } from './ItemCard';
 
-const temp: IInventory = {
-  id: '',
-  name: ItemEnum.STREAK_FREEZE,
-  description: '',
-  price: {},
-  duration: 0,
-  imageId: '',
-  image: {
-    id: '',
-    name: '',
-    owner: '',
-    permission: '',
-    url: '',
-  },
-  quantity: 0,
-  expAt: '',
-};
-
 export const Inventory = () => {
   const { t } = useTranslation('item');
   const { data, isLoading } = useInventory();
-
-  const dataWithTemp = useMemo(() => {
-    if (data && data?.length === 1 && !isLoading) {
-      return [...data, temp];
-    }
-    return data;
-  }, [data]);
 
   if (isLoading) return <Loading />;
 
@@ -54,11 +26,11 @@ export const Inventory = () => {
 
   return (
     <FlatList
-      style={{ paddingVertical: 16, margin: 'auto' }}
+      style={{ padding: 16 }}
       contentContainerStyle={{ justifyContent: 'center', gap: 16 }}
-      columnWrapperStyle={{ justifyContent: 'flex-start', gap: 16 }}
+      columnWrapperStyle={{ justifyContent: data?.length === 1 ? 'flex-start' : 'center', gap: 16 }}
       numColumns={2}
-      data={dataWithTemp}
+      data={data}
       renderItem={({ item }) => (
         <Item
           key={item.id}
