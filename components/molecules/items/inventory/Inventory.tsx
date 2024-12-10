026fccb2821632fb61aaf00ce5abd8c6e26981df
vmathu <91612623+vmathu@users.noreply.github.com>
2024-12-10 13:06:32 +0700

    fix(lap-561): inventory layout and filter data (#122)
    * feat(lap-455): use an item Streak Freeze and TGSC (#113)
    
    * feat(lap-455): use an item Streak Freeze and TGSC
    
    * fix: mission display progress for different skill type
    
    * fix(lap-455): format use item response
    
    * fix(lap-455): update inventory format
    
    * fix(lap-455): update reward for item response, streak extend showing
    
    * style(lap-455): add toast error and reward description
    
    * fix(lap-561): inventory layout and filter data
    
    * style(lap-561): inventory layout
    
    * style: inventory layout
    
    ---------
    
    Co-authored-by: HenryDo203 <dophamthanhuy2842003@gmail.com>

--- components/molecules/items/inventory/Inventory.tsx
+++ components/molecules/items/inventory/Inventory.tsx
@@ -1,44 +1,16 @@
-import { useMemo } from 'react';
 import { useTranslation } from 'react-i18next';
 import { FlatList, Text, View } from 'react-native';
 
 import InventoryEmpty from '~/assets/images/items/InventoryEmpty.svg';
 import Styles from '~/constants/GlobalStyles';
 import { useInventory } from '~/hooks/react-query/useItem';
-import { ItemEnum } from '~/lib/enums';
-import { IInventory } from '~/lib/types';
 
 import { Loading } from '../../Loading';
 import { Item } from './ItemCard';
 
-const temp: IInventory = {
-  id: '',
-  name: ItemEnum.STREAK_FREEZE,
-  description: '',
-  price: {},
-  duration: 0,
-  imageId: '',
-  image: {
-    id: '',
-    name: '',
-    owner: '',
-    permission: '',
-    url: '',
-  },
-  quantity: 0,
-  expAt: '',
-};
-
 export const Inventory = () => {
   const { t } = useTranslation('item');
   const { data, isLoading } = useInventory();
-
-  const dataWithTemp = useMemo(() => {
-    if (data && data?.length === 1 && !isLoading) {
-      return [...data, temp];
-    }
-    return data;
-  }, [data]);
 
   if (isLoading) return <Loading />;
 
@@ -54,11 +26,11 @@ export const Inventory = () => {
 
   return (
     <FlatList
-      style={{ paddingVertical: 16, margin: 'auto' }}
-      contentContainerStyle={{ justifyContent: 'center', gap: 16 }}
-      columnWrapperStyle={{ justifyContent: 'flex-start', gap: 16 }}
+      style={{ width: '100%', padding: 16 }}
+      contentContainerStyle={{ justifyContent: 'center', gap: 12 }}
+      columnWrapperStyle={{ justifyContent: 'space-between' }}
       numColumns={2}
-      data={dataWithTemp}
+      data={data}
       renderItem={({ item }) => (
         <Item
           key={item.id}
@@ -71,7 +43,6 @@ export const Inventory = () => {
         />
       )}
       keyExtractor={(item) => item.id}
-      ListFooterComponent={() => <View style={{ height: 20 }} />}
     />
   );
 };
