aa898e90dd772b8ecca9609787fb30355ce55784
HenryDo203 <dophamthanhuy2842003@gmail.com>
2025-01-22 17:02:26 +0700

    fix(lap-718): on-boarding screen

--- locales/vi/index.ts
+++ locales/vi/index.ts
@@ -1,4 +1,5 @@
 import auth from './auth.json';
+import common from './common.json';
 import item from './item.json';
 import lesson from './lesson.json';
 import milestone from './milestone.json';
@@ -6,6 +7,5 @@ import mission from './mission.json';
 import profile from './profile.json';
 import question from './question.json';
 import validation from './validation.json';
-import vietnamese from './vietnamese.json';
 
-export { auth, item, lesson, milestone, mission, profile, question, validation, vietnamese };
+export { auth, common, item, lesson, milestone, mission, profile, question, validation };
