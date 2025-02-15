1628d2a2dc68e26001b00049a188a937c9ecd743
Ngân Trúc <97326103+NganTrucLe@users.noreply.github.com>
2025-02-15 20:53:08 +0700

    style: change home image and increase touch boundary trackbar (#163)

--- services/index.ts
+++ services/index.ts
@@ -2,5 +2,4 @@ export * from './axios/auth';
 export * from './axios/bucket';
 export * from './axios/daily-lesson';
 export * from './axios/user';
-export * from './storage';
 export * from './utils';
