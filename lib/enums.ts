a586d9e2955d710e9362587a65682c990c165e51
vmathu <91612623+vmathu@users.noreply.github.com>
2025-02-17 19:11:00 +0700

    feat(LAP-765): show streak_freeze used on milestone screen (#166)
    * feat(LAP-765): show streak_freeze used on milestone screen
    
    * feat(LAP-765): show streak_freeze used on calendar in streak screen
    
    * fix(LAP-756): display freeze continuously on streak calendar
    
    * style: fix cut svg image in streak milestone
    
    ---------
    
    Co-authored-by: NganTrucLe <ngantruc2003@gmail.com>

--- lib/enums.ts
+++ lib/enums.ts
@@ -64,6 +64,7 @@ export enum ActionNameEnum {
   DAILY_LOGIN = 'daily_login',
   TASK_COMPLETED = 'task_completed',
   DAILY_STREAK = 'daily_streak',
+  FREEZE_STREAK = 'freeze_streak',
 }
 
 export enum MissionCategoryEnum {
